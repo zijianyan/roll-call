fc48b406f33a208af21fb243afb41be418b4d08e
Zijian Yan <zi@zijianyan.com>
2018-09-22 17:44:39 -0400

    Add tests for models

--- test/spec.js
+++ test/spec.js
@@ -8,27 +8,30 @@ describe('models', ()=> {
 
   beforeEach(()=> syncAndSeed());
 
-  // describe('School model', ()=> {
-
-  //   it('has three schools', async ()=> {
-  //     const schools = await School.findAll();
-  //     expect(schools.length).to.equal(3);
-  //   });
-
-  // });
+  describe('School model', ()=> {
+    it('has three schools', async ()=> {
+      const schools = await School.findAll();
+      expect(schools.length).to.equal(3);
+    });
+    it('schools have their correct students', async ()=> {
+      const [ NYU, BU, USC ] = await Promise.all([
+        School.findOne({ where: { name: 'NYU' }, include: [ Student ]}),
+        School.findOne({ where: { name: 'BU' }, include: [ Student ]}),
+        School.findOne({ where: { name: 'USC' }, include: [ Student ]})
+      ])
+      expect(NYU.students.length).to.equal(2);
+      expect(BU.students.length).to.equal(1);
+      expect(USC.students.length).to.equal(1  );
+    });
+  });
 
   describe('Student model', async ()=> {
-
-
-
-
     it('has five students', async ()=> {
       const students = await Student.findAll();
       console.log('students:', students);
       expect(students.length).to.equal(5);
     });
-
-    it('students belong to their correct schools: jane: NYU, avery: USC, sam: BU, leo: NYU, nadia: none', async ()=> {
+    it('students belong to their correct schools', async ()=> {
       const [ jane, avery, sam, leo, nadia ] = await Promise.all([
         Student.findOne({ where: { firstName: 'Jane' }, include: [ School ]}),
         Student.findOne({ where: { firstName: 'Avery' }, include: [ School ]}),
