37d19551eda15b957308f4c71696816d1c13fb72
Zijian Yan <zi@zijianyan.com>
2018-10-02 22:16:41 -0400

    Add SchoolDeleteDialog to School

--- src/components/School/School.js
+++ src/components/School/School.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 
@@ -13,44 +13,65 @@ import OtherStudentsList from './OtherStudentsList';
 import { withStyles, Typography, Divider, Button, IconButton, Tooltip } from '@material-ui/core';
 import { Delete } from '@material-ui/icons';
 
+import SchoolDeleteDialog from './SchoolDeleteDialog';
+
 const styles = {
   divider: {
     margin: '20px 0 20px 0px'
   }
 }
 
-const School = ({ school, history, deleteSchool, classes })=> {
-  window.scroll(0,0);
-  if (!school) {
+
+class School extends Component {
+  constructor() {
+    super();
+    this.state = {
+      deleteDialog: false
+    };
+    this.toggleDeleteDialog = this.toggleDeleteDialog.bind(this);
+  }
+
+  componentDidMount() {
+    window.scroll(0,0);
+  }
+
+  toggleDeleteDialog() {
+    this.setState({ deleteDialog: !this.state.deleteDialog });
+  }
+
+  render() {
+    const { school, history, deleteSchool, classes } = this.props;
+    const { toggleDeleteDialog } = this;
+    const { deleteDialog } = this.state;
+    if (!school) {
+      return (
+        <div>
+          <Typography variant='display1'>School Not Found</Typography>
+          <Link to='/schools'>See Schools</Link>
+        </div>
+      );
+    }
     return (
-      <div>
-        <Typography variant='display1'>School Not Found</Typography>
-        <Link to='/schools'>See Schools</Link>
-      </div>
+      <Fragment> 
+
+        <SchoolInfo school={school}/>
+
+        <SchoolForm type='update' history={history} school={school}/>
+
+        <Tooltip title='Delete School'><IconButton onClick={toggleDeleteDialog}><Delete /></IconButton></Tooltip>
+        
+        <Divider />
+
+        <EnrolledStudentsList schoolId={school.id}/>
+
+        <OtherStudentsList schoolId={school.id}/>
+
+        <SchoolDeleteDialog deleteDialog={deleteDialog} toggleDeleteDialog={toggleDeleteDialog} deleteSchool={deleteSchool} school={school}/>
+
+      </Fragment>
     );
   }
-  return (
-    <div> 
-
-      <SchoolInfo school={school}/>
-      
-
-
-      <SchoolForm type='update' history={history} school={school}/>
-
-      
-      
-      <Tooltip title='Delete School'><IconButton onClick={()=> deleteSchool(school)}><Delete /></IconButton></Tooltip>
-      
-      <Divider />
-
-      <EnrolledStudentsList schoolId={school.id}/>
-
-      <OtherStudentsList schoolId={school.id}/>
-
-    </div>
-  );
-};
+}
 
 const mapStateToProps = ({ schools }, { match })=> {
   return {
